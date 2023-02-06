ecc8dd71487557073038f8ac2659ceae4aa07ae3
Umer457 <68316446+Umer457@users.noreply.github.com>
2023-02-06 16:20:38 +0500

    Added Pytest to handle exception
    Add a Pytest to handle exception when the json response fails to get data

---
+++ Assignment2/test_assignment2_requests.py
@@ -0,0 +1,29 @@
+import requests
+
+response = requests.get("http://127.0.0.1:5000/info")
+
+print(response)
+print(response.json())
+
+
+def test_get_info():
+    response = requests.get("http://127.0.0.1:5000/info")
+    assert response.status_code == 200
+    assert response.json() == {'Users': [{'email': 'umer.nadeem@gmail.com', 'name': 'Umer Nadeem'},
+                                         {'email': 'usamarashid@gmail.com', 'name': 'Usama Rashid'},
+                                         {'email': 'durazesaqib@gmail.com', 'name': 'Duraze Saqib'}]}
+
+def test_get_info_with_incorrect_url():
+    response = requests.get("http://127.0.0.1:5000//invalid")
+    assert response.status_code == 404
+
+def test_not_get_info():
+    response = requests.get("http://127.0.0.1:5000/info")
+    assert response.status_code == 200
+    try:
+        response_data = response.json()
+        assert response_data == {'Users': [{'email': 'umer.nadeem@gmail.com', 'name': 'Umer Nadeem'},
+                                           {'email': 'usamarashid@gmail.com', 'name': 'Usama Rashid'}]}
+    except Exception as e:
+        print("Error parsing JSON response:", e)
+
